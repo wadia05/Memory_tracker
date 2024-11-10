87dd6781eea9ac857a1da310b8509d83ff33a1e1
wadia05 <wadiaaitbabaarroub@gmail.com>
2024-11-10 11:05:28 +0100

    memory tracker

---
+++ track_memory/memory_tracker.c
@@ -0,0 +1,53 @@
+#include "memory_tracker.h"
+
+static tracker_t *hd_tracker = NULL; 
+
+void *tracker_malloc(size_t size)
+{
+    void *address_data = malloc(size);
+    if (address_data == NULL)
+        return NULL;
+    
+    tracker_t *node = malloc (sizeof(tracker_t));
+    if(node == NULL)
+    {
+        free(address_data);
+        return NULL;
+    }
+
+    node->address = address_data;
+    node->next_addr = hd_tracker;
+    hd_tracker = node;
+    return node->address;
+}
+void free_all_allocate()
+{
+    if (hd_tracker == NULL)
+        return;
+    
+    tracker_t *curr = hd_tracker ;
+    tracker_t *next;
+    
+    while (curr != NULL)
+    {
+        next = curr->next_addr;
+        free(curr->address);
+        free(curr);
+        curr = next;
+    }
+    hd_tracker = NULL;
+}
+
+
+int main ()
+{
+    char *str = tracker_malloc (sizeof(char));
+    str[0]='s';
+    // char *sls = tracker_malloc (sizeof(-77));
+
+    free_all_allocate();
+    // char *ll = malloc(sizeof(int)*2);
+    int *nub = tracker_malloc (sizeof(int)*2);
+    nub[0] = 2;
+    while(1){};
+}
